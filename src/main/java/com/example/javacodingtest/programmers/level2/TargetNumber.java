c0facd684c8ad047cda374125a36790ad4b67133
ranunclulus <morion002@gmail.com>
2023-06-26 16:55:34 +0900

    fix

--- src/main/java/com/example/javacodingtest/programmers/level2/TargetNumber.java
+++ src/main/java/com/example/javacodingtest/programmers/level2/TargetNumber.java
@@ -9,8 +9,8 @@ class Solution {
         // 끝까지 다 검사했을 때
         if (current == numbers.length) {
             // 타겟 넘버를 만족하면 +1
-            if(sum == target) return 1;
-            // 타겟 넘버를 만족하지 않으면 +0
+            if (sum == target) return 1;
+                // 타겟 넘버를 만족하지 않으면 +0
             else return 0;
         }
         // 다음 숫자 더했을 경우
@@ -19,12 +19,15 @@ class Solution {
         answer += dfs(numbers, target, sum - numbers[current], current + 1);
         return answer;
     }
+
     public int solution(int[] numbers, int target) {
         return dfs(numbers, target, 0, 0);
-}
-public class TargetNumber {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution(new int[] {1, 1, 1, 1}, 3);
+    }
+
+    public class TargetNumber {
+        public static void main(String[] args) {
+            Solution solution = new Solution();
+            solution.solution(new int[]{1, 1, 1, 1}, 3);
+        }
     }
 }
