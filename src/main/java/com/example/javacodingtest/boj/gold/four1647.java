2284dd881a7cf0ca5445beab23f1250532180778
ranunclulus <morion002@gmail.com>
2023-07-23 21:14:36 +0900

    feat: boj gold four 1647

--- src/main/java/com/example/javacodingtest/boj/gold/four1647.java
+++ src/main/java/com/example/javacodingtest/boj/gold/four1647.java
@@ -4,110 +4,84 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-class node implements Comparable<node>{
-    int start, end, weight;
-    public node(int start, int end, int weight) {
+class node implements Comparable<node> {
+    int start;
+    int end;
+    int cost;
+
+    public node(int start, int end, int cost) {
         this.start = start;
         this.end = end;
-        this.weight = weight;
+        this.cost = cost;
     }
 
     @Override
     public int compareTo(node o) {
-        // TODO Auto-generated method stub
-        return this.weight-o.weight;
+        return this.cost - o.cost;
     }
 }
-
 public class four1647 {
-    int[] parent;
-
+    private int[] parent;
     public void solution() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         // Node, Edge Tokenizer
-        StringTokenizer neTokenizer = new StringTokenizer(reader.readLine());
-        int nodeCount = Integer.parseInt(neTokenizer.nextToken());
-        int edgeCount = Integer.parseInt(neTokenizer.nextToken());
+        String[] infoToken = reader.readLine().split(" ");
+        int nodeCount = Integer.parseInt(infoToken[0]);
+        int edgeCount = Integer.parseInt(infoToken[1]);
 
-        // Kruskal Algorithm은 서로소 집합의 개념을 이용해 서로 다른 두 정점을
-        // 연결했을 때 사이클이 발생하는지 검사
-        // 배열로 표현한 트리
-        parent = new int[nodeCount];
+        // Kruskal Algorithm은 서로소 집합의 개념을 이용해 서로 다른 두 정점을 연결했을 때의 사이클 검사
+        parent = new int[nodeCount + 1];
 
-        // 각각의 원소들이 자신을 대표자로
-        for (int i = 0; i < nodeCount; i++) {
+        // 각각의 원소들이 자신을 대표자로 하는 집합으로 만들기
+        for (int i = 0; i <= nodeCount; i++) {
             parent[i] = i;
         }
 
         // 간선 정보 해독
-        PriorityQueue<node> edges = new PriorityQueue<node>();
+        PriorityQueue<node> edges = new PriorityQueue<>();
         for (int i = 0; i < edgeCount; i++) {
-            StringTokenizer edgeTokenizer = new StringTokenizer(reader.readLine());
-            int start = Integer.parseInt(edgeTokenizer.nextToken())-1;
-            int end = Integer.parseInt(edgeTokenizer.nextToken())-1;
-            int weight = Integer.parseInt(edgeTokenizer.nextToken());
-            edges.add(new node(start, end, weight));
-
+            String[] edgeToken = reader.readLine().split(" ");
+            int start = Integer.parseInt(edgeToken[0]);
+            int end = Integer.parseInt(edgeToken[1]);
+            int cost = Integer.parseInt(edgeToken[2]);
+            edges.add(new node(start, end, cost));
         }
 
 
-        // 간선들을 가중치 순서대로 순회하면서 고른다
-        int picked = 0;
-        int totalWeight = 0;
+        // 간선들을 가중치 순서대로 순회하면서 고르기
+        int max = 0; // 선택한 간선 중 가장 큰 비용
+        int totalWeight = 0; // 총 비용
 
         while(!edges.isEmpty()) {
-            node nowEdge = edges.poll();
-            if (findSet(nowEdge.start) != findSet(nowEdge.end)) {
-                union(nowEdge.start, nowEdge.end);
-                totalWeight += nowEdge.weight;
-                picked++;
+            node current = edges.poll();
+            int startParent = findSet(current.start);
+            int endParent = findSet(current.end);
+            if (startParent != endParent) {
+                union(current.start, current.end);
+                totalWeight += current.cost;
+                max = current.cost;
             }
-            if (picked + 2 == nodeCount) break;
         }
-        System.out.println(totalWeight);
+        System.out.println(totalWeight - max);
     }
+
+    // union: 둘을 합하는 연산
     public void union(int start, int end) {
-        parent[findSet(end)] = findSet(start);
+        int startParent = findSet(start);
+        int endParent = findSet(end);
+        parent[endParent] = startParent;
     }
+
+    // findSet: 내 부모가 나일 때까지 재귀 호출
     public int findSet(int node) {
-        if(parent[node] != node) {
-            return findSet(parent[node]);
+        if (parent[node] != node) {
+            return parent[node] = findSet(parent[node]);
         }
-        return node;
+        else return node;
     }
+
     public static void main(String[] args) throws IOException {
         new four1647().solution();
     }
-
 }
-/*
-7 12
-1 2 3
-1 3 2
-3 2 1
-2 5 2
-3 4 4
-7 3 6
-5 1 5
-1 6 2
-6 4 1
-6 5 3
-4 5 3
-6 7 4
- */
-
-/*
-7 9
-1 2 29
-1 5 75
-2 3 35
-2 6 34
-3 4 7
-4 6 23
-4 7 13
-5 6 53
-6 7 25
- */
-
