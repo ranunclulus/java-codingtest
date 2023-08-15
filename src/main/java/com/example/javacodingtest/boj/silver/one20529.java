8c3c128e739e7f9f6ab66ab82e5738c17f4693d5
ranunclulus <morion002@gmail.com>
2023-08-15 23:50:23 +0900

    feat: bog gold 20529

--- src/main/java/com/example/javacodingtest/boj/silver/one20529.java
+++ src/main/java/com/example/javacodingtest/boj/silver/one20529.java
@@ -3,7 +3,6 @@ package com.example.javacodingtest.boj.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class one20529 {
@@ -12,12 +11,38 @@ public class one20529 {
         int testCnt = Integer.parseInt(reader.readLine());
         
         for (int test = 0; test < testCnt; test++) {
-            
             int personCnt = Integer.parseInt(reader.readLine());
-            String[][] personMbti = new String[personCnt][4];
-            StringTokenizer mbtiToken = new StringTokenizer(reader.readLine());
-            
 
+            String[] personMbti = new String[personCnt];
+            StringTokenizer mbtiToken = new StringTokenizer(reader.readLine(), " ");
+
+            // 입력받기
+            for (int i = 0; i < personCnt; i++) {
+                personMbti[i] = mbtiToken.nextToken();
+            }
+
+            if (personCnt > 32) {
+                System.out.println(0);
+                continue;
+            }
+
+            int minDistance = Integer.MAX_VALUE;
+
+            for (int first = 0; first < personCnt; first++) {
+                for (int second = first + 1; second < personCnt; second++) {
+                    for (int third = second + 1; third < personCnt; third++) {
+                        int temp = 0;
+                        for (int alpha = 0; alpha < 4; alpha++) {
+                            temp += (personMbti[first].charAt(alpha) != personMbti[second].charAt(alpha) ? 1 : 0);
+                            temp += (personMbti[second].charAt(alpha) != personMbti[third].charAt(alpha) ? 1 : 0);
+                            temp += (personMbti[first].charAt(alpha) != personMbti[third].charAt(alpha) ? 1 : 0);
+                        }
+                        minDistance = Math.min(temp, minDistance);
+                        if (minDistance == 0) break;
+                    }
+                }
+            }
+            System.out.println(minDistance);
         }
     }
 
