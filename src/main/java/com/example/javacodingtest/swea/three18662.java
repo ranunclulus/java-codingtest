8aeb3932a041c45ad7b5c9a7583ac29a2cbac685
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-11 01:41:39 +0900

    feat: swea d3 18662

--- src/main/java/com/example/javacodingtest/swea/three18662.java
+++ src/main/java/com/example/javacodingtest/swea/three18662.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class three18662 {
-    private int difference;
     private int[] numbers;
 
     public void solution() throws IOException {
@@ -12,37 +11,12 @@ public class three18662 {
         int testNum = sc.nextInt();
 
         for (int test = 0; test < testNum; test++) {
-            difference = -1;
-
             numbers = new int[3];
             for (int i = 0; i < 3; i++) {
-                numbers[i] = sc.nextInt() * 10;
+                numbers[i] = sc.nextInt();
             }
-
-            boolean moreToSearch = true;
-
-            while (moreToSearch) {
-                difference += 1;
-                int[] diff = new int[]{difference, -difference};
-                for (int i = 0; i < 2; i++) {
-                    int first = numbers[0] + diff[i];
-                    for (int j = 0; j < 2; j++) {
-                        int second = numbers[1] + diff[j];
-                        for (int k = 0; k < 2; k++) {
-                            int third = numbers[2] + diff[k];
-                            if ((third - second) == (second - first)) {
-                                moreToSearch = false;
-                                break;
-                            }
-                        }
-                        if (!moreToSearch) break;
-                    }
-                    if (!moreToSearch) break;
-                }
-                if (!moreToSearch) break;
-            }
-
-            System.out.printf("#%d %.1f\n", test + 1, difference / 10.0);
+            double difference = numbers[1] - ((numbers[0] + numbers[2]) / 2.0);
+            System.out.printf("#%d %.1f\n", test + 1, Math.abs(difference));
         }
     }
 
