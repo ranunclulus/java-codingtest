e4aa2df04dd0ce1e77a9ea267fcadc212d38bb26
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-10 15:07:37 +0900

    feat: boj gold five 14503

--- src/main/java/com/example/javacodingtest/boj/gold/five14503.java
+++ src/main/java/com/example/javacodingtest/boj/gold/five14503.java
@@ -3,25 +3,13 @@ package com.example.javacodingtest.boj.gold;
 import java.io.IOException;
 import java.util.Scanner;
 
-class Machine {
-    int col;
-    int row;
-    int direction;
-
-    public Machine(int col, int row, int direction) {
-        this.col = col;
-        this.row = row;
-        this.direction = direction;
-    }
-}
-
 public class five14503 {
     private int colNum;
     private int rowNum;
     private int[][] room;
-    private int[] dCol = new int[]{1, 0, -1, 0};
-    private int[] dRow = new int[]{0, -1, 0, 1};
-    private int result;
+    private int[] dCol = new int[]{-1, 0, 1, 0};
+    private int[] dRow = new int[]{0, 1, 0, -1};
+    private int result = 1;
 
     public void solution() throws IOException {
         Scanner sc = new Scanner(System.in);
@@ -32,7 +20,6 @@ public class five14503 {
         int machineCol = sc.nextInt();
         int machineRow = sc.nextInt();
         int machineDir = sc.nextInt();
-        Machine machine = new Machine(machineCol, machineRow, machineDir);
 
         for (int i = 0; i < colNum; i++) {
             for (int j = 0; j < rowNum; j++) {
@@ -40,54 +27,36 @@ public class five14503 {
             }
         }
 
-        while (true) {
-            // boolean isUpdated = false;
-            if (room[machine.col][machine.row] == 0) {
-                result++;
-                room[machine.col][machine.row] = 1;
-            }
-            if (allClean(machine)) {
-                if (machine.col == 0 || machine.col == colNum - 1
-                        || machine.row == 0 || machine.row == rowNum - 1) {
-                    break;
-                }
-                machine.col += dCol[machine.direction];
-                machine.row += dRow[machine.direction];
-                //isUpdated = true;
-            } else {
-                machine.direction = (machine.direction + 3) % 4;
-                int nextCol = machine.col - dCol[machine.direction];
-                int nextRow = machine.row - dRow[machine.direction];
+        clean(machineCol, machineRow, machineDir);
 
-                if (nextCol < 0 || nextCol >= colNum
-                        || nextRow < 0 || nextRow >= rowNum - 1) {
-                    continue;
-                } else if (room[nextCol][nextRow] == 0) {
-                    machine.col = nextCol;
-                    machine.row = nextRow;
-                }
-            }
-        }
         System.out.println(result);
     }
 
-    // 청소되지 않은 빈칸이 있으면 false
-    private boolean allClean(Machine machine) {
+    private void clean(int machineCol, int machineRow, int machineDir) {
+        room[machineCol][machineRow] = -1;
+
         for (int i = 0; i < 4; i++) {
-            int nextCol = machine.col + dCol[i];
-            int nextRow = machine.row + dRow[i];
+            machineDir = (machineDir + 3) % 4;
+            int nextCol = machineCol + dCol[machineDir];
+            int nextRow = machineRow + dRow[machineDir];
 
             if (nextCol < 0 || nextCol >= colNum || nextRow < 0 || nextRow >= rowNum) {
                 continue;
             }
-
             if (room[nextCol][nextRow] == 0) {
-                return false;
+                result++;
+                clean(nextCol, nextRow, machineDir);
+                return;
             }
         }
-        return true;
-    }
 
+        int backDirection = (machineDir + 2) % 4;
+        int backCol = machineCol + dCol[backDirection];
+        int backRow = machineRow + dRow[backDirection];
+        if (backCol >= 0 && backCol < colNum && backRow >= 0 && backRow < rowNum && room[backCol][backRow] != 1) {
+            clean(backCol, backRow, machineDir);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         new five14503().solution();
