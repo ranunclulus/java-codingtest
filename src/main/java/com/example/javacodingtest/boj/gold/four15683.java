2ae21196bd7d146640dfe4beef92d0062a398175
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-17 16:03:05 +0900

    feat: boj gold four 15683

--- src/main/java/com/example/javacodingtest/boj/gold/four15683.java
+++ src/main/java/com/example/javacodingtest/boj/gold/four15683.java
@@ -1,123 +1,130 @@
 package com.example.javacodingtest.boj.gold;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-// https://www.acmicpc.net/problem/15683
+class CCTV {
+    int col;
+    int row;
+    int num;
+
+    CCTV(int col, int row, int num) {
+        this.col = col;
+        this.row = row;
+        this.num = num;
+    }
+}
+
 public class four15683 {
-    private int suspected;
     private int colNum;
     private int rowNum;
+    private int[] output;
     private int[][] map;
+    private int[][] copyMap;
+    private List<CCTV> cctvList = new ArrayList<>();
     private int[] dCol = new int[]{-1, 0, 1, 0};
     private int[] dRow = new int[]{0, 1, 0, -1};
-    private int[][] cctvInfo = new int[][]{{0, 1, 0, 0},
-            {0, 1, 0, 1},
-            {1, 1, 0, 0},
-            {1, 1, 0, 1},
-            {1, 1, 1, 1}};
-    private boolean[][] watched;
+    private int result = Integer.MAX_VALUE;
 
     public void solution() throws IOException {
         Scanner sc = new Scanner(System.in);
         colNum = sc.nextInt();
         rowNum = sc.nextInt();
         map = new int[colNum][rowNum];
-        watched = new boolean[colNum][rowNum];
 
         for (int i = 0; i < colNum; i++) {
             for (int j = 0; j < rowNum; j++) {
                 map[i][j] = sc.nextInt();
-            }
-        }
-
-        suspected = 0;
-        for (int i = 0; i < colNum; i++) {
-            for (int j = 0; j < rowNum; j++) {
                 if (map[i][j] != 0 && map[i][j] != 6) {
-                    watched[i][j] = true;
-                    suspected++;
-                    direction(i, j);
-                }
-                if (map[i][j] == 6 && !watched[i][j]) {
-                    watched[i][j] = true;
-                    suspected++;
+                    cctvList.add(new CCTV(i, j, map[i][j]));
                 }
             }
         }
 
+        output = new int[cctvList.size()];
+        permutaion(0, cctvList.size());
+
+        if (result == Integer.MAX_VALUE) result = 0;
+        System.out.println(result);
+    }
+
+    private void permutaion(int depth, int size) {
+        if (depth == size) {
+            copyMap = new int[colNum][rowNum];
+            for (int i = 0; i < colNum; i++) {
+                for (int j = 0; j < rowNum; j++) {
+                    copyMap[i][j] = map[i][j];
+                }
+            }
+
+            for (int i = 0; i < cctvList.size(); i++) {
+                direction(cctvList.get(i), output[i]);
+            }
+
+            getBlindSpot();
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            output[depth] = i;
+            permutaion(depth + 1, size);
+        }
+    }
+
+    private void getBlindSpot() {
         int count = 0;
         for (int i = 0; i < colNum; i++) {
             for (int j = 0; j < rowNum; j++) {
-                if (map[i][j] == 0 && !watched[i][j]) count++;
+                if (copyMap[i][j] == 0) count++;
             }
         }
-
-        System.out.println(count);
+        result = Math.min(result, count);
     }
 
-    private void direction(int col, int row) {
-        int[] cctv = cctvInfo[map[col][row] - 1];
-        List<int[]>[] findMax = new List[4];
+    private void direction(CCTV cctv, int d) {
+        int cctvNum = cctv.num;
 
-        // cctv 네 번 회전
-        for (int i = 0; i < 4; i++) {
-            findMax[i] = new ArrayList<>();
-
-            // 네 방향 검사
-            for (int j = 0; j < 4; j++) {
-                int rawCol = col;
-                int rawRow = row;
-                // 해당 방향의 카메라가 아니라면
-                if (cctv[j] == 0) continue;
-                else {
-                    while (true) {
-                        int nextCol = rawCol + dCol[j];
-                        int nextRow = rawRow + dRow[j];
-
-                        if (nextCol < 0 || nextCol >= colNum || nextRow < 0 || nextRow >= rowNum || map[nextCol][nextRow] == 6) {
-                            break;
-                        }
-
-                        if (!watched[nextCol][nextRow]) {
-                            findMax[i].add(new int[]{nextCol, nextRow});
-                            watched[nextCol][nextRow] = true;
-                        }
-                        rawCol = nextCol;
-                        rawRow = nextRow;
-                    }
-                }
-            }
-            cctv = turnRight(cctv);
+        if (cctvNum == 1) {
+            watch(cctv, d);
+        } else if (cctvNum == 2) {
+            watch(cctv, d);
+            watch(cctv, (d + 2) % 4);
+        } else if (cctvNum == 3) {
+            watch(cctv, d);
+            watch(cctv, (d + 1) % 4);
+        } else if (cctvNum == 4) {
+            watch(cctv, d);
+            watch(cctv, (d + 1) % 4);
+            watch(cctv, (d + 3) % 4);
+        } else if (cctvNum == 5) {
+            watch(cctv, 0);
+            watch(cctv, 1);
+            watch(cctv, 2);
+            watch(cctv, 3);
         }
-
-        int max = Integer.MIN_VALUE;
-        int maxIndex = -1;
-        for (int i = 0; i < 4; i++) {
-            if (findMax[i].size() > max) {
-                max = findMax[i].size();
-                maxIndex = i;
-            }
-        }
-
-        for (int i = 0; i < 4; i++) {
-            if (i == maxIndex) continue;
-            for (int j = 0; j < findMax[i].size(); j++) {
-                watched[findMax[i].get(j)[0]][findMax[i].get(j)[1]] = false;
-            }
-        }
-        suspected += max;
     }
 
-    private int[] turnRight(int[] cctv) {
-        int temp = cctv[3];
-        cctv[3] = cctv[2];
-        cctv[2] = cctv[1];
-        cctv[1] = cctv[0];
-        cctv[0] = temp;
-        return cctv;
+    private void watch(CCTV cctv, int d) {
+        Queue<int[]> toVisit = new LinkedList<>();
+        toVisit.add(new int[]{cctv.col, cctv.row});
+
+        while (!toVisit.isEmpty()) {
+            int[] now = toVisit.poll();
+            int nextCol = now[0] + dCol[d];
+            int nextRow = now[1] + dRow[d];
+
+            if (nextCol < 0 || nextCol >= colNum
+                    || nextRow < 0 || nextRow >= rowNum
+                    || copyMap[nextCol][nextRow] == 6) {
+                break;
+            }
+
+            toVisit.add(new int[]{nextCol, nextRow});
+
+            if (copyMap[nextCol][nextRow] == 0) {
+                copyMap[nextCol][nextRow] = 7;
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
