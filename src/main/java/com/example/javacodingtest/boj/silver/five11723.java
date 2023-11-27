2d3efe6cb3f26b1d122a8282319bca3480a24ccd
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-11-27 13:22:09 +0900

    feat: boj silver five 11723

--- src/main/java/com/example/javacodingtest/boj/silver/five11723.java
+++ src/main/java/com/example/javacodingtest/boj/silver/five11723.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class five11723 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
