967e1980cec485b9129ca0eb35e31863571dff37
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-12-17 23:35:50 +0900

    feat: boj bronze five 1271

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/five1271.java
@@ -0,0 +1,16 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class five1271 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = in.nextBigInteger();
+        BigInteger m = in.nextBigInteger();
+        in.close();
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+
+    }
+}
