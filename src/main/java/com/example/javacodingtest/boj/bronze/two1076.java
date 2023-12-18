235266140bc0794324bea96ac7efd4fc1bffee73
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-12-18 18:06:35 +0900

    feat: boj bronzw two

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/two1076.java
@@ -0,0 +1,25 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class two1076 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] color = {"black", "brown", "red", "orange", "yellow", "green",
+                "blue", "violet", "grey", "white"};
+
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+
+        long result = (Arrays.asList(color).indexOf(A) * 10) + Arrays.asList(color).indexOf(B);
+        result *= Math.pow(10, Arrays.asList(color).indexOf(C));
+
+        System.out.println(result);
+    }
+
+}
