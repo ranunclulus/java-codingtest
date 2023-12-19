27f37f954268cea966c35aee22b3b5db0308ca2a
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-12-20 06:38:51 +0900

    feat: boj bronze one 1037

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/one1037.java
@@ -0,0 +1,25 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.util.Scanner;
+
+public class one1037 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        while (T-- > 0) {
+            int N = in.nextInt();
+            max = N > max ? N : max;
+            min = N < min ? N : min;
+
+        }
+        System.out.println(max * min);
+    }
+
+}
