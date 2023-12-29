442ea9df8cef6c3a2fd312cd59bdf3fcc71c95ad
Huisu <87214089+ranunclulus@users.noreply.github.com>
2023-12-29 22:12:31 +0900

    feat: boj bronze one 2163

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/one2163.java
@@ -0,0 +1,19 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.util.Scanner;
+
+public class one2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int count = 0;
+
+        count = m * n - 1;
+        System.out.println(count);
+
+    }
+
+}
