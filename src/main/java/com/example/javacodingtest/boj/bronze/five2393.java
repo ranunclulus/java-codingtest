7e15fbfbeaed314f0552a07483f9fd7364966085
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-02-14 22:28:25 +0900

    feat: boj bronze five 2393

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/five2393.java
@@ -0,0 +1,19 @@
+package com.example.javacodingtest.boj.bronze;
+
+public class five2393 {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/"); // \는 앞에 \ 한번 더 필요함
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
