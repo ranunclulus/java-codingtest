71eee54012bd820a10960c6db15331af05960cf3
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-02-15 11:22:21 +0900

    feat: boj bronze five 4999

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/five4999.java
@@ -0,0 +1,18 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.util.Scanner;
+
+public class five4999 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        if (a.length() >= b.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
