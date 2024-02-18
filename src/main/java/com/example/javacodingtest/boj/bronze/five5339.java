a551ffab9780572774c4b6563496ccc235310621
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-02-18 23:58:24 +0900

    feat: boj bronze five 5339

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/five5339.java
@@ -0,0 +1,18 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.IOException;
+
+public class five5339 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
