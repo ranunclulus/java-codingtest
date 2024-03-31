aba5af179c3bb83670b29ee4b3881c97988f390d
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-03-31 21:11:31 +0900

    feat: windmill codingTest FittingBlock

---
+++ src/main/java/com/example/javacodingtest/codingtest/windmill/FittingBlock.java
@@ -0,0 +1,15 @@
+package com.example.javacodingtest.codingtest.windmill;
+
+import java.util.Scanner;
+
+public class FittingBlock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        boolean[][] map = new boolean[m][n];
+
+    }
+
+}
