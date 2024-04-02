0ede8c6b8e52bc600dd8ec6b0003030c98c6e3cf
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-04-02 10:39:05 +0900

    feat: boj silver five 14916 예외 처리

--- src/main/java/com/example/javacodingtest/boj/silver/five14916.java
+++ src/main/java/com/example/javacodingtest/boj/silver/five14916.java
@@ -9,15 +9,18 @@ public class five14916 {
         int coin = 0;
 
         coin = cost / 5;
-        cost %= 5;
+        int rest = cost % 5;
 
-        if (cost % 2 == 1) {
-            cost += 5;
+        if (rest % 2 == 1 && cost > 5) {
+            rest += 5;
             coin--;
         }
-        coin += cost / 2;
 
-        System.out.println(coin);
+        coin += rest / 2;
+        rest %= 2;
+
+        if (rest == 0) System.out.println(coin);
+        else System.out.println(-1);
     }
 
     public static void main(String[] args) throws Exception {
