2022c6625c3a706adc3d220cd626c54ecf77367e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-04-26 17:01:27 +0900

    feat: boj bronze three 2455

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2455.java
@@ -0,0 +1,26 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class three2455 {
+    public void solution() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int current = 0;
+
+        for (int i = 0; i < 4; i++) {
+            StringTokenizer infoToken = new StringTokenizer(sc.nextLine());
+            int output = Integer.parseInt(infoToken.nextToken());
+            int input = Integer.parseInt(infoToken.nextToken());
+            current = current + input - output;
+            max = Math.max(max, current);
+        }
+        System.out.println(max);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new three2455().solution();
+    }
+}
