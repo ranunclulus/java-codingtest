42332d46af6b0d821dc14de50a12c9c892dc7cf8
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-04-29 23:35:04 +0900

    feat: boj bronze three2522:

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2522.java
@@ -0,0 +1,36 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class three2522 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new three2522().solution();
+    }
+}
