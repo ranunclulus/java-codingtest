b3ef966fcec26bdf9df28eb24aca8368515d9329
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-05-06 11:29:15 +0900

    featL boj bronze two1718

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/two1718.java
@@ -0,0 +1,45 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class two1718 {
+    public void solution() throws IOException {
+        List<Character> s = new ArrayList<>();
+        List<Character> c = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+        String code = sc.next();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            s.add(sentence.charAt(i));
+        }
+        for (int i = 0; i < code.length(); i++) {
+            c.add(code.charAt(i));
+        }
+        int z = 0;
+        char j = ' ';
+        char[] cd = new char[sentence.length()];
+        for (int i = 0; i < cd.length; i++) {
+            cd[i] = s.get(i);
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s.size() > c.size()) {
+                c.add(c.get(i));
+            }
+            z = c.get(i) - 'a' + 1;
+            cd[i] = (char) (s.get(i) - z);
+            if (cd[i] < 'a') cd[i] += 26;
+            if (s.get(i) == ' ') cd[i] = ' ';
+        }
+        for (char e : cd) System.out.print(e + "");
+    }
+
+    public static void main(String[] args) throws IOException {
+        new two1718().solution();
+    }
+}
