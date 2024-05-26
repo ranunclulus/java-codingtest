20ec036e83f994e469ab475f178bf42fc41ade05
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-05-26 10:14:10 +0900

    feat: boj silver three 2193

---
+++ src/main/java/com/example/javacodingtest/boj/silver/three2193.java
@@ -0,0 +1,28 @@
+package com.example.javacodingtest.boj.silver;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class three2193 {
+    int n;
+    long[] answer;
+    int cnt;
+
+    public void solution() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        answer = new long[n + 1];
+        answer[0] = 0;
+        answer[1] = 1;
+        answer[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+        System.out.println(answer[n]);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new three2193().solution();
+    }
+}
