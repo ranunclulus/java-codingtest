4d585f68774c0c637b646303f32693b80f6c00eb
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-05-29 19:33:40 +0900

    feat: boj bronze three1837

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three1837.java
@@ -0,0 +1,30 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class three1837 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger P = new BigInteger(st.nextToken());
+        BigInteger K = new BigInteger(st.nextToken());
+        BigInteger i = new BigInteger("1");
+
+        while (true) {
+            if (i.compareTo(K) == 1 || i.compareTo(K) == 0) {
+                System.out.println("GOOD");
+                break;
+            }
+            i = i.add(BigInteger.ONE);
+
+            if (P.remainder(i).compareTo(BigInteger.ZERO) == 0 && i.compareTo(K) == -1) {
+                System.out.println("BAD " + i);
+                break;
+            }
+        }
+    }
+}
