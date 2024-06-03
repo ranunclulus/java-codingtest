d0087d1c55ec9a42b9d64cee9ea1b870027a3f5e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-03 20:59:21 +0900

    feat: boj bronze two 1350

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/two1350.java
@@ -0,0 +1,30 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class two1350 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long count = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long size = Long.parseLong(br.readLine());
+        for (int i = 0; i < N; i++) {
+            long file = Long.parseLong(st.nextToken());
+
+            if (file != 0) {
+                count += (file / size);
+                if (file % size != 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(size * count);
+    }
+
+}
