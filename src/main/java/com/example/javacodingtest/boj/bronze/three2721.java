adf33fd54cc2736591e455a6c11c3804c2aec3ba
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-04 15:48:10 +0900

    feat: boj bronze three 2721

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2721.java
@@ -0,0 +1,18 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.util.Scanner;
+
+public class three2721 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            int tot = 0;
+            for (int j = 1; j <= n; j++) {
+                tot += j * (j + 1) * (j + 2) / 2;
+            }
+            System.out.println(tot);
+        }
+    }
+}
