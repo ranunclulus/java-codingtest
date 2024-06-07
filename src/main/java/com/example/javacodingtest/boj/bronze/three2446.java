58638295f6fc91bd8362434b21383b209d156f97
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-07 09:53:26 +0900

    three2446.java 업데이트

--- src/main/java/com/example/javacodingtest/boj/bronze/three2446.java
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2446.java
@@ -9,15 +9,12 @@ public class three2446 {
 
         int N = in.nextInt();
 
-        // 역삼각형 코드
         for (int i = 0; i < N; i++) {    // 1 ~ N
 
-            // 공백
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
 
-            // 별
             for (int k = 0; k < (2 * N - 1) - (2 * i); k++) {
                 System.out.print("*");
             }
@@ -25,15 +22,12 @@ public class three2446 {
             System.out.println();
         }
 
-        // 삼각형 코드
         for (int i = 0; i < N - 1; i++) {    // N+1 ~ 2N-1
 
-            // 공백
             for (int j = 1; j < (N - 1) - i; j++) {
                 System.out.print(" ");
             }
 
-            // 별
             for (int k = 0; k < 3 + 2 * i; k++) {
                 System.out.print("*");
             }
