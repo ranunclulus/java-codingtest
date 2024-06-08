7398edcff5910e0458176d878f5ee4d3d3726ad3
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-08 23:35:15 +0900

    feat: boj bronze three 2783

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2783.java
@@ -0,0 +1,29 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class three2783 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double X = Double.parseDouble(st.nextToken());
+        double Y = Double.parseDouble(st.nextToken());
+        double R = (1000 / Y) * X;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            double x = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
+            double r = (1000 / y) * x;
+
+            R = Math.min(R, r);
+        }
+        System.out.println(String.format("%.2f", R));
+    }
+
+}
