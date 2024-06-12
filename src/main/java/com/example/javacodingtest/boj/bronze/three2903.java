fe6dd68e8c1759fffa4a78a12011309ace59121b
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-12 23:47:33 +0900

    feat: boj bronze three2903

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2903.java
@@ -0,0 +1,16 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class three2903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        System.out.println((int) Math.pow(Math.pow(2, n) + 1, 2));
+    }
+}
