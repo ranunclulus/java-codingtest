83985fb0f4548ab42e2780c24e9d970964f7b1d4
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-06-18 22:58:33 +0900

    feat: boj bronze three 2985

---
+++ src/main/java/com/example/javacodingtest/boj/bronze/three2985.java
@@ -0,0 +1,35 @@
+package com.example.javacodingtest.boj.bronze;
+
+import java.util.Scanner;
+
+public class three2985 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a + b == c) {
+            System.out.println(a + "+" + b + "=" + c);
+        } else if (a - b == c) {
+            System.out.println(a + "-" + b + "=" + c);
+        } else if (a * b == c) {
+            System.out.println(a + "*" + b + "=" + c);
+        } else if (a / b == c) {
+            System.out.println(a + "/" + b + "=" + c);
+        }
+
+        if (a == b + c) {
+            System.out.println(a + "=" + b + "+" + c);
+        } else if (a == b - c) {
+            System.out.println(a + "=" + b + "-" + c);
+        } else if (a == b * c) {
+            System.out.println(a + "=" + b + "*" + c);
+        } else if (a == b / c) {
+            System.out.println(a + "=" + b + "/" + c);
+        }
+        sc.close();
+    }
+}
