94a67caa331a371145647e62a2f4871f97b6f8d1
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-03 10:19:44 +0900

    feat: boj gold four 4485

--- src/main/java/com/example/javacodingtest/boj/gold/four4485.java
+++ src/main/java/com/example/javacodingtest/boj/gold/four4485.java
@@ -1,36 +1,85 @@
 package com.example.javacodingtest.boj.gold;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/*
+ @author ranuinclulus
+ @since
+ @link https://www.acmicpc.net/problem/4485
+ @timecomplex
+ @performance 19356 KB, 188 MS
+ @category
+ @note
+ */
 public class four4485 {
-    private int[][] adjMat;
+    class Node implements Comparable<Node>{
+        int row;
+        int col;
+        int cost;
 
+        public Node(int row, int col, int cost) {
+            this.row = row;
+            this.col = col;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder builder = new StringBuilder();
+    static StringTokenizer tokenizer;
+    static int n;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[][] distance;
+    static int[][] deltas = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int answer;
     public void solution() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int nodes = Integer.parseInt(reader.readLine());
-        int problemNum = 1;
-        while (nodes != 0) {
-            this.adjMat = new int[nodes][nodes];
+        int testNum = 0;
+        while(true) {
+            testNum++;
+            n = Integer.parseInt(reader.readLine());
+            if (n == 0) break;
 
-            for (int i = 0; i < nodes; i++) {
-                StringTokenizer edgeToken = new StringTokenizer(reader.readLine());
-                for (int j = 0; j < nodes; j++) {
-                    adjMat[i][j] = Integer.parseInt(edgeToken.nextToken());
+            map = new int[n][n];
+            distance = new int[n][n];
+            visited = new boolean[n][n];
+            for (int i = 0; i < n; i++) {
+                tokenizer = new StringTokenizer(reader.readLine());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(tokenizer.nextToken());
+                    distance[i][j] = Integer.MAX_VALUE;
                 }
             }
-            for (int[] row: adjMat
-                 ) {
-                System.out.println(Arrays.toString(row));
+            distance[0][0] = map[0][0];
+
+            PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+            priorityQueue.offer(new Node(0, 0, distance[0][0]));
+            visited[0][0] = true;
+
+            while(!priorityQueue.isEmpty()) {
+                Node node = priorityQueue.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nextRow = node.row + deltas[i][1];
+                    int nextCol = node.col + deltas[i][0];
+
+                    if (nextCol < 0 || nextCol >= n || nextRow < 0 || nextRow >= n) continue;
+                    if (visited[nextRow][nextCol]) continue;
+
+                    visited[nextRow][nextCol] = true;
+                    distance[nextRow][nextCol] = node.cost + map[nextRow][nextCol];
+                    priorityQueue.add(new Node(nextRow, nextCol, distance[nextRow][nextCol]));
+                }
             }
-
-
-            nodes = Integer.parseInt(reader.readLine());
-            problemNum++;
+            builder.append("Problem ").append(testNum).append(": ").append(distance[n - 1][n - 1]).append('\n');
         }
+        writer.write(builder.toString());
+        writer.flush();
     }
 
     public static void main(String[] args) throws IOException {
