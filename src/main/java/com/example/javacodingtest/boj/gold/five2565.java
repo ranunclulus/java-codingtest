8c472f3d1e6c7d9f0d916a4765a7210721146a9b
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-09-28 11:58:11 +0900

    feat: commit

---
+++ src/main/java/com/example/javacodingtest/boj/gold/five2565.java
@@ -0,0 +1,31 @@
+package com.example.javacodingtest.boj.gold;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+ @author ranuinclulus
+ @since
+ @link https://www.acmicpc.net/problem/2565
+ @timecomplex
+ @performance
+ @category
+ @note
+ */
+public class five2565 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder builder = new StringBuilder();
+    static StringTokenizer tokenizer;
+    static int n, answer;
+    static int[] dp = new int[101];
+
+    public void solution() throws IOException {
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new five2565().solution();
+    }
+}
