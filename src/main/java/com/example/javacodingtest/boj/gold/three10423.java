ef8691321d5c12d9862f9294237e47aec1fdff9e
Huisu <87214089+ranunclulus@users.noreply.github.com>
2024-11-06 10:10:57 +0900

    feat: gold three 10423

--- src/main/java/com/example/javacodingtest/boj/gold/three10423.java
+++ src/main/java/com/example/javacodingtest/boj/gold/three10423.java
@@ -1,19 +1,21 @@
 package com.example.javacodingtest.boj.gold;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 /*
  @author ranuinclulus
- @since 2024.11.04
- @link https://www.acmicpc.net/problem/1368
+ @since 2024.11.05
+ @link https://www.acmicpc.net/problem/10423
  @timecomplex
- @performance 25508kb 304ms
+ @performance 48032kb 436ms
  @category
  @note
  */
-public class two1368 {
-    class Edge implements Comparable<Edge>{
+public class three10423 {
+    class Edge implements Comparable<Edge> {
         int start;
         int end;
         int cost;
@@ -33,42 +35,49 @@ public class two1368 {
     static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder builder = new StringBuilder();
     static StringTokenizer tokenizer;
-    static int n;
-    static int[] costs, parents;
     static PriorityQueue<Edge> priorityQueue;
-    static int minCost;
+    static int n, m, k, totalCost;
+    static int[] parents, power;
 
     public void solution() throws IOException {
-        n = Integer.parseInt(reader.readLine());
-        costs = new int[n + 1];
-        parents = new int[n + 1];
+        tokenizer = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+        k = Integer.parseInt(tokenizer.nextToken());
 
+        parents = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            costs[i] = Integer.parseInt(reader.readLine());
             parents[i] = i;
         }
 
+        power = new int[k];
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < k; i++) {
+            power[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        for (int i = 0; i < k; i++) {
+            parents[power[i]] = power[0];
+        }
 
         priorityQueue = new PriorityQueue<>();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < m; i++) {
             tokenizer = new StringTokenizer(reader.readLine());
-            for (int j = 1; j <= n; j++) {
-                int value = Integer.parseInt(tokenizer.nextToken());
-                if (i == j) priorityQueue.add(new Edge(0, i, costs[i]));
-                else if (i < j) priorityQueue.add(new Edge(i, j, value));
-            }
+            priorityQueue.add(new Edge(
+                    Integer.parseInt(tokenizer.nextToken()),
+                    Integer.parseInt(tokenizer.nextToken()),
+                    Integer.parseInt(tokenizer.nextToken())));
         }
 
         while (!priorityQueue.isEmpty()) {
             Edge now = priorityQueue.poll();
-
-            if (find(now.start) == find(now.end)) continue;
-            union(now.start, now.end);
-            minCost += now.cost;
+            if (find(now.start) != find(now.end)) {
+                union(now.start, now.end);
+                totalCost += now.cost;
+            }
         }
 
-
-        builder.append(minCost);
+        builder.append(totalCost);
         writer.write(builder.toString());
         writer.flush();
     }
@@ -81,12 +90,12 @@ public class two1368 {
         else parents[startParent] = endParent;
     }
 
-    private int find(int node) {
-        if (parents[node] == node) return node;
-        else return parents[node] = find(parents[node]);
+    private int find(int target) {
+        if (parents[target] == target) return target;
+        else return parents[target] = find(parents[target]);
     }
 
     public static void main(String[] args) throws IOException {
-        new two1368().solution();
+        new three10423().solution();
     }
 }
